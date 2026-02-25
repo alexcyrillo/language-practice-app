86bb25302161970c97de7c02fe545860a4eb5496
Alex Cyrillo <alex.csborges@gmail.com>
2026-02-25 21:46:02 +0000

    refatorar funções de manipulação de banco de dados para usar decks e melhorar a estrutura das consultas

--- src/database/db.py
+++ src/database/db.py
@@ -5,66 +5,87 @@ load_dotenv()
 
 def init():
   db_name = os.getenv("DB_NAME")
-  connection = sqlite3.connect(f"data/{db_name}")
-
-  connection.execute('''
-  CREATE TABLE IF NOT EXISTS question_tb (
-                     id INTEGER PRIMARY KEY AUTOINCREMENT,
-                     question TEXT NOT NULL UNIQUE,
-                     answer TEXT NOT NULL,
-                     creation_date DATETIME DEFAULT CURRENT_TIMESTAMP
-                     )
-  ''')
-
-  connection.commit()
-  connection.close()
+  try:
+    connection = sqlite3.connect(f"data/{db_name}")
+    connection.execute('''
+    CREATE TABLE IF NOT EXISTS decks (
+                      id INTEGER PRIMARY KEY,
+                      name TEXT NOT NULL UNIQUE
+                      )
+    ''')
+    connection.commit()
+    print("Banco de Dados Conectado")
+  except:
+    print("Erro ao Conectar Banco de Dados")
+  finally:
+    connection.close()
 
 def command(sql_command, values = None):
   db_name = os.getenv("DB_NAME")
-  connection = sqlite3.connect(f"data/{db_name}")
-  cursor = connection.cursor()
-  if (values != None):
-    cursor.execute(sql_command, values)
-  else:
-    cursor.execute(sql_command)
-  result = cursor.fetchall()
-  connection.commit()
-  connection.close()
-  return result
+  try:
+    connection = sqlite3.connect(f"data/{db_name}")
+    cursor = connection.cursor()
+    if (values != None):
+      cursor.execute(sql_command, values)
+    else:
+      cursor.execute(sql_command)
+    result = cursor.fetchall()
+    connection.commit()
+    return result
+  except:
+    print("Erro ao Conectar Banco de Dados")
+  finally:
+    connection.close()
 
-def create_question(question, answer):
-  sql = "INSERT INTO question_tb (question, answer) VALUES (?, ?);"
-  values = (question, answer)
+def create_deck(deck_name):
+  # possivel sql injection, verificar
+  sql = f'''CREATE TABLE IF NOT EXISTS "{deck_name}" ( id INTEGER PRIMARY KEY AUTOINCREMENT, question TEXT NOT NULL UNIQUE, answer TEXT NOT NULL);'''
+  command(sql)
+
+  # registra na tabela de decks criados
+  sql = "INSERT INTO decks (name) VALUES (?)"
+  values = (deck_name, )
   command(sql, values)
 
-def get_question(id):
-  sql = "SELECT id, question, answer FROM question_tb WHERE id = ?;"
-  values = (id, )
-  result = command(sql, values)
-  return result
-
-def list_questions():
-  sql = "SELECT id, question, answer FROM question_tb;"
+def list_decks():
+  sql = "SELECT id, name FROM decks;"
   result = command(sql)
   return result
 
-def edit_question(id, question, answer):
-  sql = "UPDATE question_tb SET question = ?, answer = ? WHERE id = ?;"
+def create_question(question, answer, deck):
+  sql = f'''INSERT INTO "{deck}" (question, answer) VALUES (?, ?);'''
+  values = (question, answer)
+  command(sql, values)
+
+def get_question(id, deck):
+  sql = "SELECT id, question, answer FROM ? WHERE id = ?;"
+  values = (id, deck)
+  result = command(sql, values)
+  return result
+
+def list_questions(deck):
+  sql = "SELECT id, question, answer FROM ?;"
+  values = (deck, )
+  result = command(sql, values)
+  return result
+
+def edit_question(id, question, answer, deck):
+  sql = f'''UPDATE "{deck}" SET question = ?, answer = ? WHERE id = ?;'''
   values = (question, answer, id)
   command(sql, values)
 
-def delete_question(id):
-  sql = "DELETE FROM question_tb WHERE id = ?;"
-  values = (id, )
+def delete_question(id, deck):
+  sql = f'''DELETE FROM "{deck}" WHERE id = ?;'''
+  values = (id)
   command(sql, values)
 
-def list_aleatory_questions(num_of_question):
+def list_aleatory_questions(num_of_question, deck):
   result = None
   if(num_of_question > 0):
-    sql = "SELECT question, answer FROM question_tb ORDER BY RANDOM() LIMIT ?;"
+    sql = f'''SELECT question, answer FROM "{deck}" ORDER BY RANDOM() LIMIT ?;'''
     values = (num_of_question, )
     result = command(sql, values)
   else:
-    sql = "SELECT question, answer FROM question_tb ORDER BY RANDOM();"
+    sql = f'''SELECT question, answer FROM "{deck}" ORDER BY RANDOM();'''
     result = command(sql)
   return result
