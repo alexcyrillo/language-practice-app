8cc2e9fd3f04e7be262857a840985df6764c74be
Alex Cyrillo <alex.csborges@gmail.com>
2026-02-25 21:47:19 +0000

    refatorar função de revisão para melhorar a lógica de comparação de respostas e adicionar contagem de acertos e erros

--- src/core/review.py
+++ src/core/review.py
@@ -1,25 +1,38 @@
 from database import db
+from core.select_from_list import select_from_list
+import re
 
-def compare_answer(question, answer):
-  if(question == answer):
-    return True
-  else:
-    return False
+def review(deck_name):
+  decks = db.list_decks()
+  correct_count = 0
+  incorrect_count = 0
 
-def review():
-  # falta tratamenro de erro 
   review_qt = int(input("Quantas palavras deseja revisar? (0 - Para revisar tudo)\n"))
 
-  result = db.list_aleatory_questions(review_qt)
+  cards = db.list_aleatory_questions(review_qt, deck_name)
 
-  for el in result:
-    question = el[0]
-    correct_answer = el[1]
+  for card in cards:
+    question = card[0]
+    correct_answer = card[1]
     print("---------------------------")
     user_answer = input(f"{question}\n")
 
-    if(compare_answer(correct_answer, user_answer)):
-      print("Correto")
+    correct_answer_splited = correct_answer.split(";")
+    for i, el in enumerate(correct_answer_splited):
+      clean_el = re.sub(r'\(.*?\)', '', el)
+      correct_answer_splited[i] = clean_el.strip().lower()
+
+    if user_answer in correct_answer_splited:
+      print(f"Correto!\n{correct_answer}")
+      correct_count += 1
     else:
       print(f" Incorreto\n Correto: {correct_answer}\n Sua resposta: {user_answer}")
+      incorrect_count += 1
     print("---------------------------")
+  
+    keep = input("Pressione qualquer botao para continuar; q para sair")
+    if(keep == "q"):
+      break
+
+  print(f"\n-----Pontuação Final-----\nCorretas: {correct_count}\nIncorretas: {incorrect_count}\n-------------------------")
+      
